665dcf7d24309198f8cec4ec75b052b3d46136c4
PizzaYx <371812114@qq.com>
2023-12-28 17:40:25 +0800

    1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -14,8 +14,18 @@
 					<image src="../../static/resource/images/modal_closer.png"
 						style="display: block;width: 15px;height: 15px;"></image>
 				</view>
-
 			</view>
+		</view>
+
+		<view v-if="slides && slides.length > 0" class="index-swiper">
+			<swiper autoplay circular :interval="4000" :duration="500">
+				<block v-for="(item,index) in slides" :key="index">
+					<swiper-item>
+						<image :src="item.pic_image_url" mode="widthFix" show-menu-by-longpress :data-index="index">
+						</image>
+					</swiper-item>
+				</block>
+			</swiper>
 		</view>
 	</view>
 </template>
@@ -24,22 +34,53 @@
 	import { ref, reactive, computed } from 'vue'
 	import { onLoad } from '@dcloudio/uni-app'
 
-	onLoad(() => {
-		console.log(app.globalData);
-	})
-	const data = ref('动态数据')
-	const changeData = (val) => {
-		data.value = val
-	}
-
 	const app = getApp()
-	const navigatorTo = () => {
-		uni.navigateTo({
-			url: '/pages/search/index'
+
+	//定义轮播图数据
+	const slides = ref([])
+
+	onLoad(() => {
+		app.globalData.utils.getUserInfo()
+		app.globalData.utils.request({
+			url: '/app/init',
+			success: res => {
+				console.log(res.data);
+				const { id } = res.data.area
+				//通过id去获取当前地区的页面数据
+				app.globalData.utils.request({
+					url: '/Index/index',
+					data: {
+						aid: id
+					},
+					success: ({ data }) => {
+						console.log(data);
+						slides.value = data.slides
+					}
+				})
+			}
 		})
-	}
+	})
 </script>
 
 <style>
-	page {}
+	page {
+		background: #fff;
+	}
+
+	.index-swiper {
+		padding: 20rpx 20rpx 0 20rpx;
+		overflow: hidden;
+
+	}
+
+	.index-swiper swiper {
+		height: 320rpx;
+		overflow: hidden;
+		border-radius: 10rpx;
+	}
+
+	.index-swiper swiper-item image {
+		width: 100%;
+		height: 100%;
+	}
 </style>
